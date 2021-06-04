342b271b7d490b7b25b6ad127deea9a2e72210b6
phongngo715 <85334788+phongngo715@users.noreply.github.com>
2021-06-04 22:26:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const CONFIG = {
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích phong <33',
     btnNo: 'Không, À nha  :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao linh lại yêu phong <3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao Linh lại yêu Phong <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Phong biết mà 🥰. Yêu linh nhiều Lắm luôn😘😘',
